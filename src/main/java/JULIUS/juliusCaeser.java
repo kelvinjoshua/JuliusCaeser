436c19ffde03cb6792292496fd154a6c28a8cc0d
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-14 17:18:09 +0300

    Added branching logic to ensureshift value ranges only 0 to 26

--- src/main/java/JULIUS/juliusCaeser.java
+++ src/main/java/JULIUS/juliusCaeser.java
@@ -6,11 +6,28 @@ public class juliusCaeser {
     private int ShiftValue;
     private String input;
     /*constructor*/
-    public String juliusCaeser(String input, int shiftValue){
+    public String juliusCaeser(String input, int ShiftValue){
         this.input= input;
         this.ShiftValue= ShiftValue;
+    /* value range fixed between 0 and 23*/
+    if(ShiftValue >26){
+        ShiftValue= ShiftValue%26;
+    }
+    else if(ShiftValue <0){
+        ShiftValue= (ShiftValue%26) +26;
+    }
 
 
+
+
+
+
+
+
+
+
+
+       return encryptedMessage;
     }
 
 
--- src/test/java/JULIUS/juliusCaeserTest.java
+++ src/test/java/JULIUS/juliusCaeserTest.java
@@ -1,7 +1,17 @@
 package JULIUS;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class juliusCaeserTest {
+        /* @Test
+        public void juliusCaeser_instanceOfClass_true() throws Exception {
+            juliusCaeser object = new juliusCaeser();
+            String expectedOutput= object.juliusCaeser("kelvin",24);
+            assertEquals(true,expectedOutput instanceof juliusCaeser);
+
+        }*/
+
 
 }
