1f4b4e0adcdea3a06eaee9819afbde3d8ea21c15
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-14 16:57:21 +0300

    Defined properties for blueprint class and its constructor

---
+++ src/main/java/JULIUS/App.java
@@ -0,0 +1,9 @@
+package JULIUS;
+import java.io.Console;
+public class App {
+    public static void main(String[] args){
+        Console terminal = System.console();
+
+
+    }
+}
--- src/main/java/JULIUS/juliusCaeser.java
+++ src/main/java/JULIUS/juliusCaeser.java
@@ -1,4 +1,17 @@
 package JULIUS;
 
 public class juliusCaeser {
+    /*leave as primitives*/
+    private String encryptedMessage = "";
+    private int ShiftValue;
+    private String input;
+    /*constructor*/
+    public String juliusCaeser(String input, int shiftValue){
+        this.input= input;
+        this.ShiftValue= ShiftValue;
+
+
+    }
+
+
 }
