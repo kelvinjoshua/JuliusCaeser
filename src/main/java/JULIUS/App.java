731f2e7197673dbe651bb218ab127599f6ce1904
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-14 18:08:36 +0300

    Added logic to the UI to prompt user for the key

--- src/main/java/JULIUS/App.java
+++ src/main/java/JULIUS/App.java
@@ -4,6 +4,10 @@ public class App {
     public static void main(String[] args){
         Console terminal = System.console();
         System.out.println("Enter the message you want to encrypt");
+        String message = terminal.readLine();
+
+        System.out.println("Enter the shift value");
+        String digit = terminal.readLine();
 
 
     }
