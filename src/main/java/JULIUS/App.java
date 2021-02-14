628b551a41180341df1b4da90718c4af95070e21
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-14 18:05:22 +0300

    Added logic to the UI to prompt user input

--- src/main/java/JULIUS/App.java
+++ src/main/java/JULIUS/App.java
@@ -3,6 +3,7 @@ import java.io.Console;
 public class App {
     public static void main(String[] args){
         Console terminal = System.console();
+        System.out.println("Enter the message you want to encrypt");
 
 
     }
